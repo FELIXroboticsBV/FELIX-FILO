46567fd55e4cc35a738b50a079952a170d63ee59
Simon <simon.potocnak03@gmail.com>
2026-09-16 11:43:21 +0200

    renamed icons

--- packaging/AppImage-builder/create_appimage.py
+++ packaging/AppImage-builder/create_appimage.py
@@ -62,7 +62,7 @@ def copy_files(dist_path):
     """
     copied_files = {
         os.path.join("..", "icons", "felix-filo-icon-256-256.svg"): os.path.join("usr", "share", "icons", "hicolor", "scalable", "apps", "felix-filo-icon.svg"),
-        os.path.join("..", "icons", "felix-filo-icon-64x64.png"): os.path.join("usr", "share", "icons", "hicolor", "64x64", "apps", "felix-filo-icon.pngy"),
+        os.path.join("..", "icons", "felix-filo-icon-64x64.png"): os.path.join("usr", "share", "icons", "hicolor", "64x64", "apps", "felix-filo-icon.png"),
         os.path.join("..", "icons", "felix-filo-icon-128x128.png"): os.path.join("usr", "share", "icons", "hicolor", "128x128", "apps", "felix-filo-icon.png"),
         os.path.join("..", "icons", "felix-filo-icon-128-128.ico"): os.path.join("usr", "share", "icons", "hicolor", "256x256", "apps", "felix-filo-icon.png"),
         os.path.join("..", "icons", "felix-filo-icon-256-256.ico"): "felix-filo-icon.png",
